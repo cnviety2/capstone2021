9b33cb65e6ca29b73599b757d39fa24619925fd4
ChuongNQSE <chuongnqse63246@fpt.edu.vn>
2021-07-07 12:49:50 +0700

    RecruiterController
    Add getAllJob() trong RecruiterController

---
+++ Capstone2021/DTO/Job.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Capstone2021.DTO
+{
+    public class Job
+    {
+        public int id { get; set; }
+        public string jobName { get; set; }
+        public int workingForm { get; set; }
+        public int location { get; set; }
+        public string workingPlace { get; set; }
+        public string description { get; set; }
+        public string requirement { get; set; }
+        public bool type { get; set; }
+        public string offer { get; set; }
+        public bool sex { get; set; }
+        public int quantity { get; set; }
+        public int salaryMin { get; set; }
+        public int salaryMax { get; set; }
+        public int recruiterId { get; set; }
+        public DateTime createDate { get; set; }
+        public int managerId { get; set; }
+    }
+}
---
+++ Capstone2021/Services/RecruiterServiceImpl.cs
@@ -0,0 +1,53 @@
+﻿using Capstone2021.DTO;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone2021.Services
+{
+    public class RecruiterServiceImpl : RecruiterService, IDisposable
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        private DbEntities context;
+        public RecruiterServiceImpl()
+        {
+            context = new DbEntities();
+        }
+        public void Dispose()
+        {
+           context.Dispose();
+        }
+
+        public Job getAllJob(int id)
+        {
+            Job result = null;
+            using (context)
+            {
+                result = context.jobs.AsEnumerable().Where(c => c.recruiter_id == id).Select(c => new Job()
+                {
+                    id = c.id,
+                    jobName = c.name,
+                    recruiterId = c.recruiter_id,
+                    createDate = c.create_date,
+                    description = c.description,
+                    location = c.location,
+                    workingPlace = c.working_place,
+                    salaryMin=c.salary_min,
+                    salaryMax = c.salary_max,
+                    offer=c.offer,
+                    quantity=c.quantity,
+                    workingForm=c.working_form,
+                    requirement=c.requirement,
+                }).FirstOrDefault<Job>();
+            }
+            return result;
+        }
+
+        public Job PostAJob(PostAJobDTO model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

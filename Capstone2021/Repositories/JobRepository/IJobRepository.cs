eaa2426da9d91ab76f0152f427d247d5540d9111
tran duy <cnviety2@gmail.com>
2021-07-10 20:19:13 +0700

    update Job,add JobController,JobService,JobServiceImpl,IJobRepository,JobMapper,chinh sua database

---
+++ Capstone2021/Repositories/JobRepository/IJobRepository.cs
@@ -0,0 +1,15 @@
+﻿using Capstone2021.DTO;
+using Capstone2021.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone2021.Repositories.JobRepository
+{
+    interface IJobRepository : IBasicRepository<Job>
+    {
+
+    }
+}
